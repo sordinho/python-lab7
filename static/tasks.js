16ee876edfc16547c9430c552ba4e218b0d2f9e4
sordinho <davi.sordi96@gmail.com>
2018-05-07 19:44:25 +0200

    added update task... need to know how to modify function called on submit

--- static/tasks.js
+++ static/tasks.js
@@ -9,7 +9,8 @@ function loadList() {
         var tasklist = data["tasks"];
         for (var i = 0; i < tasklist.length; i++) {
             var t = tasklist[i];
-            $("#tasklist ul").append("<li><button id='removeTask' onclick='removeTask(this)' data-id='" + t.id + "'>Delete</button> <button id='updateTask'>Update</button> t.description t.urgent </li>");
+            $("#tasklist ul").append("<li><button id='removeTask' onclick='removeTask(this)' data-id='" + t.id + "'>Delete</button> " +
+                "<button id='updateTask' onclick='updateTask(this)' data-id='" + t.id + "' data-descr='" + t.description + "' data-urg='" + t.urgent + "'>Update</button>" + "   " + t.description + " --- " + t.urgent + "</li>");
         }
 
 
@@ -18,24 +19,53 @@ function loadList() {
 
 function removeTask(taskElement) {
     var idTask = taskElement.getAttribute("data-id");
-    alert(idTask);
+    //alert(idTask);
     $.ajax({
         url: RESTAPI + "/tasks/" + idTask,
         type: 'DELETE',
 
     });
-    //location.reload();
+    location.reload();
     return false;
 }
 
-function updateTask(id,desc,urg){
-    alert(id);
-    alert(desc);
-    alert(urg);
+function updateTask(taskElement) {
+    var id = taskElement.getAttribute("data-id");
+    var desc = taskElement.getAttribute("data-descr");
+    var urg = taskElement.getAttribute("data-urg");
+    //alert(id);    alert(desc);    alert(urg);
+
+    $("input[name='description']").val(desc);
+    $("#addTask span").text('Update');
+    if (urg == 1) {
+        //alert(urg);
+        $("input[name='urgent']").attr('checked', true);
+    }
+    if (urg == 0) {
+        //alert(urg);
+        $("input[name='urgent']").attr('checked', false);
+    }
+    $("#addTask").on('click',function () {
+
+        var task = {"id": id, "description": description, "urgent": urgent};
+
+        var taskJSON = JSON.stringify(task);
+
+        $.put({
+            "url": RESTAPI + '/tasks/' + id,
+            "data": taskJSON,
+            "contentType": "application/json",
+            "success": function () {
+                loadList()
+            }
+        });
+    })
+
 }
 
 $(document).ready(function () {
     loadList();
+
     $("form").submit(function () {
         var description = $("input[name='description']").val();
         var urgent = 0;
